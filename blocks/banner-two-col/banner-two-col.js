7803de65bcb68de068e5ebf50a870bc366ef8268
dan.zhang <54833095+danzhang-pm@users.noreply.github.com>
2026-07-29 17:40:22 +0800

    Update blocks/banner-two-col/banner-two-col.js

--- blocks/banner-two-col/banner-two-col.js
+++ blocks/banner-two-col/banner-two-col.js
@@ -1,1 +1,10 @@
-404: Not Found
+export default function decorate(block) {
+  const wrapper = block.firstElementChild;
+  if (!wrapper) return;
+
+  const children = [...wrapper.children];
+  if (children.length >= 2) {
+    children[0].classList.add('banner-col-left');
+    children[1].classList.add('banner-col-right');
+  }
+}
